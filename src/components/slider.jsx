ff50f8cc4ae8a965dfa922e5516b0b843102444b
Hashir <hashirakbar07@gmail.com>
2024-02-14 20:21:42 +0500

    struggling with slider day 1

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,18 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
-function slider() {
-  const [item, setItem] = useState();
+// images
+import img1 from "../assets/hero.png";
+import { useEffect } from "react";
 
-  const sliderItems = [{}];
+function Slider() {
+  const sliderItems = [
+    {
+      item: img1,
+    },
+    {
+      item: img1,
+    },
+    {
+      item: img1,
+    },
+    {
+      item: img1,
+    },
+    {
+      item: img1,
+    },
+    {
+      item: img1,
+    },
+    {
+      item: img1,
+    },
+    {
+      item: img1,
+    },
+  ];
+  const [item, setItem] = useState(sliderItems);
+
+  const imgDiv = useRef(null);
+  let imgChilds;
+  useEffect(() => {
+    imgChilds = imgDiv.current.children[0].width;
+  }, []);
+
+  function nextClick() {
+    let slider = imgDiv.current;
+
+    slider.scroll({
+      left: 100,
+      behavior: "smooth",
+    });
+  }
 
   return (
     <div>
-      <div className="display">
-
+      <div className="display"></div>
+      <div
+        ref={imgDiv}
+        className="sliderItems  border-red-700 border flex  max-w-xl overflow-x-scroll gap-5 "
+      >
+        {item.map((item, index) => {
+          return <img src={item.item} alt="" key={index} />;
+        })}
       </div>
-      <div className="sliderItems"></div>
+      <div className="flex gap-4">
+        <button onClick={nextClick} className="border border-purple-700">
+          Next
+        </button>
+        <button className="border border-purple-700">Prev</button>
+      </div>
     </div>
   );
 }
 
-export default slider;
+export default Slider;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 // components
 import Button from "../components/button";
+import Slider from "../components/slider";
 
 const Home = () => {
   //   return <h1>Home</h1>;
-  // return <Button btnValue="Test" />;
+  return (
+    <>
+      <Slider />
+    </>
+  );
 };
 
 export default Home;
